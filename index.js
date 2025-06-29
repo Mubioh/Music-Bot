96e2619b435ea98c38cb180db269389006920c4e
Mubioh <naroberts398@gmail.com>
2025-06-29 19:14:00 +0100

    Registered stop command properly

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { Player } = require('discord-player');
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Player } = require("discord-player");
 
-const { discordToken, musicChannelId } = require('./config');
+const { discordToken, musicChannelId } = require("./config");
 
 const MusicMessages = require("./utils/MusicMessages");
 
-const playCommand = require('./commands/play');
+const playCommand = require("./commands/play");
 const stopCommand = require("./commands/stop");
 
 const {
@@ -13,20 +13,17 @@ const {
   loadEvents,
   registerCommands,
   registerPlayerEvents,
-} = require('./core/init');
+} = require("./core/init");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-  ],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
 
 client.commands = new Collection();
-client.player   = new Player(client);
+client.player = new Player(client);
 
 client.commands.set(playCommand.data.name, playCommand);
-
+client.commands.set(stopCommand.data.name, stopCommand);
 
 (async () => {
   await registerExtractors(client.player);
@@ -35,36 +32,37 @@ client.commands.set(playCommand.data.name, playCommand);
 
   loadEvents(client);
 
-  
-client.once("ready", async () => {
-  await registerCommands(client);
-  console.log(`✅ Logged in as ${client.user.tag}`);
+  client.once("ready", async () => {
+    await registerCommands(client);
+    console.log(`✅ Logged in as ${client.user.tag}`);
 
-  for (const [guildId, guild] of client.guilds.cache) {
-    const channel = guild.channels.cache.find(
-      (c) => c.id === musicChannelId && c.isTextBased?.()
-    );
+    for (const [guildId, guild] of client.guilds.cache) {
+      const channel = guild.channels.cache.find(
+        (c) => c.id === musicChannelId && c.isTextBased?.()
+      );
 
-    if (!channel) continue;
+      if (!channel) continue;
 
-    try {
-      const messages = await channel.messages.fetch({ limit: 1 });
-      const last = messages.first();
+      try {
+        const messages = await channel.messages.fetch({ limit: 1 });
+        const last = messages.first();
 
-      const embed = MusicMessages.noTrackEmbed();
-      const row = MusicMessages.controlRow({ disabled: true });
+        const embed = MusicMessages.noTrackEmbed();
+        const row = MusicMessages.controlRow({ disabled: true });
 
-      if (last?.author?.id === client.user.id) {
-        await last.edit({ embeds: [embed], components: [row] });
-      } else {
-        await channel.send({ embeds: [embed], components: [row] });
+        if (last?.author?.id === client.user.id) {
+          await last.edit({ embeds: [embed], components: [row] });
+        } else {
+          await channel.send({ embeds: [embed], components: [row] });
+        }
+      } catch (err) {
+        console.warn(
+          `Couldn’t update or send music embed in ${guild.name}:`,
+          err.message
+        );
       }
-    } catch (err) {
-      console.warn(`Couldn’t update or send music embed in ${guild.name}:`, err.message);
     }
-  }
-});
-
+  });
 
   await client.login(discordToken);
 })();
